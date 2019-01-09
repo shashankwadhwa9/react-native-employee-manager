64f50ae0e204e27475f3daff8958276bbfc14d83
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-09 21:19:03 +0530

    firebase setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from '@firebase/app';
 import reducers from './reducers';
+import {
+    firebaseApiKey,
+    firebaseAuthDomain,
+    firebaseDatabaseURL,
+    firebaseProjectId,
+    firebaseStorageBucket,
+    firebaseMessagingSenderId
+} from './settings.json';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: firebaseApiKey,
+            authDomain: firebaseAuthDomain,
+            databaseURL: firebaseDatabaseURL,
+            projectId: firebaseProjectId,
+            storageBucket: firebaseStorageBucket,
+            messagingSenderId: firebaseMessagingSenderId
+        });
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
