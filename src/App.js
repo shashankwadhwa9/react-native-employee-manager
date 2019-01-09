6272403c433f798c3b63a2d54eadc886a0fb5309
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-09 21:00:45 +0530

    create dummy reducer for now

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 class App extends Component {
     render() {
         return (
-            <Provider store={createStore()}>
+            <Provider store={createStore(reducers)}>
                 <View>
                     <Text>
                         Hello!
