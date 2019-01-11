584b9930baa5b7e9bddb7cecdaa5cde5b110bf1c
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-11 21:07:42 +0530

    clear email and password on login success

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -28,7 +28,7 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, loading: true, error: '' };
 
         case LOGIN_USER_SUCCESS:
-            return { ...state, user: action.payload, error: '', loading: false };
+            return { ...state, ...INITIAL_STATE, user: action.payload };
 
         case LOGIN_USER_FAIL:
             return { ...state, error: 'Authentication Failed.', password: '', loading: false };
