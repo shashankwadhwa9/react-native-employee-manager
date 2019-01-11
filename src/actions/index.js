92b737ff62efa8728c925c3579f80ef2017bfe93
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-11 20:20:04 +0530

    create user in firebase if login gives error

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,11 +19,17 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
     return (dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(user => {
-                dispatch({
-                    type: LOGIN_USER_SUCCESS,
-                    payload: user
-                });
+            .then(user => loginUserSuccess(dispatch, user))
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(user => loginUserSuccess(dispatch, user));
             });
     };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
 };
