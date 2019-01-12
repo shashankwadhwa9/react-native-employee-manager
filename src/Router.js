41221906692ffc68214613334c1f71f7e0b633ac
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-12 08:48:25 +0530

    add "Add" button on nav bar of employees list

--- src/Router.js
+++ src/Router.js
@@ -12,7 +12,13 @@ const RouterComponent = () => {
                 </Scene>
 
                 <Scene key="main">
-                    <Scene key="employeeList" component={EmployeeList} title="Employees" />
+                    <Scene
+                        key="employeeList"
+                        component={EmployeeList}
+                        title="Employees"
+                        rightTitle="Add"
+                        onRight={() => { console.log('right') }}
+                    />
                 </Scene>
             </Scene>
         </Router>
