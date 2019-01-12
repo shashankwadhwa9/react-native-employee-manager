6ecb51ea0541eff8a99c3a9069acee3829ec5be6
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-12 18:21:57 +0530

    dispatch action after form submit to reset state

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -4,6 +4,7 @@ import '@firebase/database';
 import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
+    EMPLOYEE_CREATE
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -16,9 +17,12 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
     const { currentUser } = firebase.auth();
 
-    return () => {
+    return (dispatch) => {
         firebase.database().ref(`/users/${currentUser.uid}/employees`)
             .push({ name, phone, shift })
-            .then(() => Actions.employeeList({ type: 'reset' }));
+            .then(() => {
+                dispatch({ type: EMPLOYEE_CREATE });
+                Actions.employeeList({ type: 'reset' });
+            });
     };
 };
