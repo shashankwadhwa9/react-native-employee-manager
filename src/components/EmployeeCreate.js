63476eb059790dbf2deab5dca63ec5b545f67e88
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-12 16:08:20 +0530

    mapStateToProps in employee create

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Card, CardSection, Input, Buttton } from './common';
+import { employeeUpdate } from '../actions';
 
 class EmployeeCreate extends Component {
     render() {
         return (
             <Card>
                 <CardSection>
-                    <Input label="Name" placeholder="Jane" />
+                    <Input
+                        label="Name"
+                        placeholder="Jane"
+                        value={this.props.name}
+                        onChangeText={
+                            value => this.props.employeeUpdate({ prop: 'name', value })
+                        }
+                    />
                 </CardSection>
 
                 <CardSection>
-                    <Input label="Phone" placeholder="555-555-5555" />
+                    <Input
+                        label="Phone"
+                        placeholder="555-555-5555"
+                        value={this.props.phone}
+                        onChangeText={
+                            value => this.props.employeeUpdate({ prop: 'phone', value })
+                        }
+                    />
                 </CardSection>
 
                 <CardSection>
@@ -26,4 +42,10 @@ class EmployeeCreate extends Component {
     }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = state => {
+    const { name, phone, shift } = state.employeeForm;
+
+    return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
