a2e0b298076304de368c66442060528926103993
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-12 19:02:36 +0530

    dispatch action after fetching employees from firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -4,7 +4,8 @@ import '@firebase/database';
 import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -23,6 +24,17 @@ export const employeeCreate = ({ name, phone, shift }) => {
             .then(() => {
                 dispatch({ type: EMPLOYEE_CREATE });
                 Actions.employeeList({ type: 'reset' });
+            });
+    };
+};
+
+export const employeesFetch = () => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
             });
     };
 };
