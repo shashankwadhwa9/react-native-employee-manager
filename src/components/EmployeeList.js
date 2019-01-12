e409313bb2348bf8ddc4ddbe6a99432e342c6ce7
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-12 22:51:46 +0530

    use FlatList instead of deprecated Listview

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { ListView } from 'react-native';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
 import ListItem from './ListItem';
@@ -8,34 +8,18 @@ import ListItem from './ListItem';
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeesFetch();
-
-        this.createDataSource(this.props);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        // nextProps are the next set of props that this component will be rendered with
-        // this.props is still the old set of props
-        this.createDataSource(nextProps);
-    }
-
-    createDataSource({ employees }) {
-        const ds = ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
-        });
-
-        this.dataSource = ds.cloneWithRows(employees);
     }
 
     renderRow(employee) {
-        return <ListItem employee={employee} />;
+        return <ListItem employee={employee.item} />;
     }
 
     render() {
         return (
-            <ListView
-                enableEmptySections
-                dataSource={this.dataSource}
-                renderRow={this.renderRow}
+            <FlatList
+                data={this.props.employees}
+                renderItem={this.renderRow}
+                keyExtractor={employee => employee.uid}
             />
         );
     }
@@ -49,6 +33,5 @@ const mapStateToProps = state => {
 
     return { employees };
 };
-
 
 export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
