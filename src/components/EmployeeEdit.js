39f4e740e20dd292622e668e77bc548aedfa0af8
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-13 08:44:34 +0530

    send text message of schedule on edit employee component

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Communications from 'react-native-communications';
 import { Card, CardSection, Button } from './common';
 import { employeeUpdate, employeeSave } from '../actions';
 import EmployeeForm from './EmployeeForm';
@@ -18,13 +19,26 @@ class EmployeeEdit extends Component {
         this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
     }
 
+    onTextPress() {
+        const { phone, shift } = this.props;
+
+        Communications.text(phone, `Your upcoming shift is on ${shift}`);
+    }
+
     render() {
         return (
             <Card>
                 <EmployeeForm {...this.props} />
+
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
                         Save
+                    </Button>
+                </CardSection>
+
+                <CardSection>
+                    <Button onPress={this.onTextPress.bind(this)}>
+                        Text Schedule
                     </Button>
                 </CardSection>
             </Card>
