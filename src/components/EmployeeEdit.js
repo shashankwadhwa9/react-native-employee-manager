d6485cd6345c5978312e40e7040a788741278408
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-13 11:01:47 +0530

    delete employee on modal confirmation

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import { Card, CardSection, Button, Confirm } from './common';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
@@ -25,6 +25,16 @@ class EmployeeEdit extends Component {
         const { phone, shift } = this.props;
 
         Communications.text(phone, `Your upcoming shift is on ${shift}`);
+    }
+
+    onAccept() {
+        const { uid } = this.props.employee;
+
+        this.props.employeeDelete({ uid });
+    }
+
+    onDecline() {
+        this.setState({ showModal: false });
     }
 
     render() {
@@ -52,6 +62,8 @@ class EmployeeEdit extends Component {
 
                 <Confirm
                     visible={this.state.showModal}
+                    onAccept={this.onAccept.bind(this)}
+                    onDecline={this.onDecline.bind(this)}
                 >
                     Are you sure you want to delete this?
                 </Confirm>
@@ -66,4 +78,4 @@ const mapStateToProps = state => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
